82e25522be93d3127bc65317252d58e7371f2d2f
Vike44 <165469270+Vike44@users.noreply.github.com>
2024-12-21 11:13:20 +0300

    Add files via upload

---
+++ Command.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    /*Реализован через DrawGraphCommand и ClearGraphCommand. 
+     * Превращает операции с графиком в объекты. 
+     * Нужен для выполнения действий асинхронно и отделения вызова операции от её реализации.*/
+    public interface IGraphCommand
+    {
+        Task Execute();
+    }
+    // Команды для работы с графиком
+    public class DrawGraphCommand : IGraphCommand
+    {
+        private readonly IGraphManager _manager;
+        private readonly string _expression;
+        private readonly double _min;
+        private readonly double _max;
+
+        public DrawGraphCommand(IGraphManager manager, string expression, double min, double max)
+        {
+            _manager = manager;
+            _expression = expression;
+            _min = min;
+            _max = max;
+        }
+
+        public async Task Execute()
+        {
+            await Task.Run(() => _manager.DrawFunction(_expression, _min, _max));
+        }
+    }
+
+    public class ClearGraphCommand : IGraphCommand
+    {
+        private readonly IGraphManager _manager;
+
+        public ClearGraphCommand(IGraphManager manager)
+        {
+            _manager = manager;
+        }
+
+        public async Task Execute()
+        {
+            await Task.Run(() => _manager.Clear());
+        }
+    }
+}
---
+++ Strategy.cs
@@ -0,0 +1,36 @@
+﻿using org.mariuszgromada.math.mxparser;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_2
+{
+    /* Калькулятор для вычисления значений функции
+     * Применяется для изоляции алгоритма вычисления значений функции.
+     * Позволяет менять способ расчета, не трогая основной код.
+     * Содержит один главный метод для вычисления значения.*/
+    public class FunctionCalculator
+    {
+        private readonly string _expression;
+
+        public FunctionCalculator(string expression)
+        {
+            _expression = expression;
+        }
+
+        public double Calculate(double x)
+        {
+            try
+            {
+                var arg = new Argument("x");
+                arg.setArgumentValue(x);
+                var expr = new Expression(_expression.Replace(',', '.'), arg);
+                return expr.calculate();
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
